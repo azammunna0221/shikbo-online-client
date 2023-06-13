3e29885390097190a05441c30c696718411c2ee5
Ali Azam <azammunna0221>
2023-06-14 01:17:00 +0600

    password Validation

--- src/Pages/SignUp/Register.jsx
+++ src/Pages/SignUp/Register.jsx
@@ -1,11 +1,20 @@
 import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { AuthContext } from "../../Provider/AuthProvider";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    const onSubmit = data => {
+        console.log(data)
+    };
+
+
+
     const { createUser } = useContext(AuthContext);
 
-    const handleCreateUser = (e) => {
+    /* const handleCreateUser = (e) => {
         e.preventDefault();
         const form = e.target;
         const firstName = form.name.value;
@@ -22,7 +31,7 @@ const Register = () => {
         }).catch(error => {
             console.log(error.message);
         })
-    }
+    } */
 
     return (
         <div>
@@ -32,36 +41,48 @@ const Register = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleCreateUser} className="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text">Full Name</span>
                                 </label>
-                                <input name="name" type="text" placeholder="Full Name" className="input input-bordered" />
+                                <input {...register("name", { required: true })} name="name" type="text" placeholder="Full Name" className="input input-bordered" />
+                                {errors.name && <span className=" text-red-500">This field is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input name="email" type="email" placeholder="Email" className="input input-bordered" />
+                                <input {...register("email",{ required: true })} name="email" type="email" placeholder="Email" className="input input-bordered" />
+                                {errors.email && <span className=" text-red-500">This field is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input name="password" type="text" placeholder="password" className="input input-bordered" />
+                                <input {...register("password",{ required: true, minLength: 6,
+                                pattern: /(?=.*?[A-Z])(?=.*?[#?!@$%^&*-])/
+                            })} 
+                                name="password" type="text" placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <span className=" text-red-500">This field is required</span>}
+                                {errors.password?.type === 'minLength' && <span className=" text-red-500">Minimum length should be 6</span>}
+                                {errors.password?.type === 'pattern' && <span className=" text-red-500"> Minimum one Uppercase,  one Special character</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Confirm Password</span>
                                 </label>
-                                <input name="confirmPassword" type="text" placeholder="password" className="input input-bordered" />
+                                <input {...register("confirmPassword", { required: true, minLength: 6,
+                                pattern: /(?=.*?[A-Z])(?=.*?[#?!@$%^&*-])/ })} name="confirmPassword" type="text" placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <span className=" text-red-500">This field is required</span>}
+                                {errors.password?.type === 'minLength' && <span className=" text-red-500">Minimum length should be 6</span>}
+                                {errors.password?.type === 'pattern' && <span className=" text-red-500"> Minimum one Uppercase,  one Special character</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo URL</span>
                                 </label>
-                                <input name="url" type="text" placeholder="URL" className="input input-bordered" />
+                                <input {...register("url")} name="url" type="text" placeholder="URL" className="input input-bordered" />
                             </div>
                             <div className="form-control mt-6">
                                 <button type="submit" className="btn btn-primary">Register</button>
